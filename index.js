7e0675ea5ef4b889383128b44a0543a4414ebb2f
Jeremy Che <jeremy.che48@gmail.com>
2018-01-28 10:46:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,10 +17,9 @@ function nowServing(katzDeliLine) {
 
 function currentLine(katzDeliLine) {
   if(katzDeliLine.length === 0) {
-    var string = katzDeliLine.shift()
-    var call = `Currently serving ${string}.`
-  } else {
     var call = "There is nobody waiting to be served!"
+  } else {
+    var string = ""
   }
   return call
 }
