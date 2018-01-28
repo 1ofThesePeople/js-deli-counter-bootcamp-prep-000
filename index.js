1cdb5168abf54896a73ad5c88afe8f860404f2df
Jeremy Che <jeremy.che48@gmail.com>
2018-01-28 10:35:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber(katzDeliLine, customer) {
-  var string = `Welcome, ${customer}. You are number ${katzDeliLine.length.toString()}`
+  var string = `Welcome, ${customer}. You are number ${katzDeliLine.length.toString() + 1} in line.`
 
   return string
 }
