e13d5cef8f3d43f67dcd88e6bdf18a72e696ed82
Jeremy Che <jeremy.che48@gmail.com>
2018-01-28 10:43:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ function takeANumber(katzDeliLine, customer) {
   var string = `Welcome, ${customer}. You are number ${number.toString()} in line.`
   return string
 }
+
+function nowServing(katzDeliLine) {
+  if(katzDeliLine.length > 0) {
+    var string = katzDeliLine.shift()
+    var call = ""
+    return ""
+  } else {
+    var string = "There is nobody waiting to be served!"
+  }
+  return string
+}
