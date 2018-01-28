daaec30c2f79b61c32e727929f8400c0a8219f60
Jeremy Che <jeremy.che48@gmail.com>
2018-01-28 10:37:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function takeANumber(katzDeliLine, customer) {
   var number = katzDeliLine.length + 1
-  console.log(number);
   var string = `Welcome, ${customer}. You are number ${number.toString()} in line.`
   return string
 }
